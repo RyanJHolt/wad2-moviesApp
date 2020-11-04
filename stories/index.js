3a3a55775004e2d6ad071350be8b488525f8a97e
Ryan Holt <44439777+RyanJHolt@users.noreply.github.com>
2020-11-04 11:58:07 +0000

    "Fix storybook"

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import MovieCard from "../src/components/movieCard";
 import FilterControls from "../src/components/filterControls";
 import MoviesHeader from "../src/components/headerMovieList";
 import MovieList from "../src/components/movieList";
 import MovieDetails from "../src/components/movieDetails";
-import MovieHeader from '../src/components/headerMovie'
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-
+import MovieHeader from "../src/components/headerMovie";
+import AddFavoriteButton from "../src/components/buttons/addToFavorites";
+import { MemoryRouter } from "react-router";
+import GenresContextProvider from "../src/contexts/genresContext";
+import { action } from "@storybook/addon-actions";
 
 const sample = {
   adult: false,
@@ -38,13 +41,13 @@ const sample = {
     }
   ],
   homepage:
-    "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
+      "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
   id: 181808,
   imdb_id: "tt2527336",
   original_language: "en",
   original_title: "Star Wars: The Last Jedi",
   overview:
-    "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+      "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
   popularity: 44.208,
   poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
   production_companies: [
@@ -91,29 +94,61 @@ const sample = {
 };
 
 storiesOf("Home Page/MovieCard", module)
-  .add("default", () => <MovieCard movie={sample} />)
-  .add("exception", () => {
-    const sampleNoPoster = { ...sample, poster_path: undefined };
-    return <MovieCard movie={sampleNoPoster} />;
-  });
+    .addDecorator(story => (
+        <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+    ))
+    .add("default", () => (
+        <MovieCard
+            movie={sample}
+            action={movie => <button className="btn w-100 btn-primary">Test</button>}
+        />
+    ))
+    .add("exception", () => {
+      const sampleNoPoster = { ...sample, poster_path: undefined };
+      return (
+          <MovieCard
+              movie={sampleNoPoster}
+              action={movie => (
+                  <button className="btn w-100 btn-primary">Test</button>
+              )}
+          />
+      );
+    });
 
 storiesOf("Home Page/FilterControls", module)
-.add("default", () => <FilterControls /> )
+    .addDecorator(story => (
+        <GenresContextProvider>{story()}</GenresContextProvider>
+    ))
+    .add("default", () => (
+        <FilterControls onUserInput={action("button-click")} numMovies={10} />
+    ));
 
 storiesOf("Home Page/Header", module).add("default", () => (
-  <MoviesHeader numMovies={10} />
+    <MoviesHeader title="All Movies" numMovies={10} />
 ));
 
 storiesOf("Home Page/MovieList", module)
-  .add("default", () => {
-    const movies= [sample, sample, sample, sample, sample]
-    return <MovieList movies={movies} />
-});
+    .addDecorator(story => (
+        <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+    ))
+    .add("default", () => {
+      const movies = [sample, sample, sample, sample, sample];
+      return (
+          <MovieList
+              movies={movies}
+              action={movie => (
+                  <button className="btn w-100 btn-primary">Test</button>
+              )}
+          />
+      );
+    });
 
 storiesOf("Movie Details Page/MovieDetails", module).add("default", () => (
-  <MovieDetails movie={sample} />
+    <MovieDetails movie={sample} />
 ));
 
-storiesOf("Movie Details Page/MovieHeader", module).add("default", () => (
-  <MovieHeader movie={sample} />
-));
+storiesOf("Movie Details Page/MovieHeader", module)
+    .addDecorator(story => (
+        <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+    ))
+    .add("default", () => <MovieHeader movie={sample} />);
